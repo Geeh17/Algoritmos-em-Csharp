06cec010e7dba22bab4f1b9dfb66a1d368767b6e
Geraldo Luiz <113297903+Geeh17@users.noreply.github.com>
2022-10-13 19:58:42 -0300

    Add files via upload
    Algoritmo par ou impar

---
+++ proj_06/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Entre com um numero inteiro: ");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("Numero Par!");
+            }
+            else { Console.WriteLine("Numero Impar!");
+            }
+
+        }
+    }
+}
